e88ca3de3e9903d209ec4fb45fcf3fe861a83346
AbdelmalekBouguerra <abdelmalekbouguerra2000@gmail.com>
2022-01-27 15:01:43 +0100

    deleting the navbar folder

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,87 +1,69 @@
-$(function() {
-
-  var siteSticky = function() {
-		$(".js-sticky-header").sticky({topSpacing:0});
-	};
-	siteSticky();
-
-	var siteMenuClone = function() {
-
-		$('.js-clone-nav').each(function() {
-			var $this = $(this);
-			$this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
-		});
 
 
-		setTimeout(function() {
-			
-			var counter = 0;
-      $('.site-mobile-menu .has-children').each(function(){
-        var $this = $(this);
-        
-        $this.prepend('<span class="arrow-collapse collapsed">');
-
-        $this.find('.arrow-collapse').attr({
-          'data-toggle' : 'collapse',
-          'data-target' : '#collapseItem' + counter,
-        });
-
-        $this.find('> ul').attr({
-          'class' : 'collapse',
-          'id' : 'collapseItem' + counter,
-        });
-
-        counter++;
-
-      });
-
-    }, 1000);
-
-		$('body').on('click', '.arrow-collapse', function(e) {
-      var $this = $(this);
-      if ( $this.closest('li').find('.collapse').hasClass('show') ) {
-        $this.removeClass('active');
-      } else {
-        $this.addClass('active');
-      }
-      e.preventDefault();  
-      
-    });
-
-		$(window).resize(function() {
-			var $this = $(this),
-				w = $this.width();
-
-			if ( w > 768 ) {
-				if ( $('body').hasClass('offcanvas-menu') ) {
-					$('body').removeClass('offcanvas-menu');
-				}
+(() =>{
+ 
+	const openNavMenu = document.querySelector(".open-nav-menu"),
+	closeNavMenu = document.querySelector(".close-nav-menu"),
+	navMenu = document.querySelector(".nav-menu"),
+	menuOverlay = document.querySelector(".menu-overlay"),
+	mediaSize = 991;
+  
+	openNavMenu.addEventListener("click", toggleNav);
+	closeNavMenu.addEventListener("click", toggleNav);
+	// close the navMenu by clicking outside
+	menuOverlay.addEventListener("click", toggleNav);
+  
+	function toggleNav() {
+		navMenu.classList.toggle("open");
+		menuOverlay.classList.toggle("active");
+		document.body.classList.toggle("hidden-scrolling");
+	}
+  
+	navMenu.addEventListener("click", (event) =>{
+		if(event.target.hasAttribute("data-toggle") && 
+			window.innerWidth <= mediaSize){
+			// prevent default anchor click behavior
+			event.preventDefault();
+			const menuItemHasChildren = event.target.parentElement;
+		  // if menuItemHasChildren is already expanded, collapse it
+		  if(menuItemHasChildren.classList.contains("active")){
+			  collapseSubMenu();
+		  }
+		  else{
+			// collapse existing expanded menuItemHasChildren
+			if(navMenu.querySelector(".menu-item-has-children.active")){
+			  collapseSubMenu();
 			}
-		})
-
-		$('body').on('click', '.js-menu-toggle', function(e) {
-			var $this = $(this);
-			e.preventDefault();
-
-			if ( $('body').hasClass('offcanvas-menu') ) {
-				$('body').removeClass('offcanvas-menu');
-				$this.removeClass('active');
-			} else {
-				$('body').addClass('offcanvas-menu');
-				$this.addClass('active');
-			}
-		}) 
-
-		// click outisde offcanvas
-		$(document).mouseup(function(e) {
-	    var container = $(".site-mobile-menu");
-	    if (!container.is(e.target) && container.has(e.target).length === 0) {
-	      if ( $('body').hasClass('offcanvas-menu') ) {
-					$('body').removeClass('offcanvas-menu');
-				}
-	    }
-		});
-	}; 
-	siteMenuClone();
-
-});
+			// expand new menuItemHasChildren
+			menuItemHasChildren.classList.add("active");
+			const subMenu = menuItemHasChildren.querySelector(".sub-menu");
+			subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+		  }
+		}
+	});
+	function collapseSubMenu(){
+		navMenu.querySelector(".menu-item-has-children.active .sub-menu")
+		.removeAttribute("style");
+		navMenu.querySelector(".menu-item-has-children.active")
+		.classList.remove("active");
+	}
+	function resizeFix(){
+		 // if navMenu is open ,close it
+		 if(navMenu.classList.contains("open")){
+			 toggleNav();
+		 }
+		 // if menuItemHasChildren is expanded , collapse it
+		 if(navMenu.querySelector(".menu-item-has-children.active")){
+			  collapseSubMenu();
+	   }
+	}
+  
+	window.addEventListener("resize", function(){
+	   if(this.innerWidth > mediaSize){
+		   resizeFix();
+	   }
+	});
+  
+  })();
+  
+  
