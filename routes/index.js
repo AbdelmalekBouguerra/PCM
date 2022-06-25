c4761ea0bbb47635c0b5ac53991eee13f41fd983
AbdelmalekBouguerra <abdelmalekbouguerra2000@gmail.com>
2022-06-25 15:35:09 +0100

    working test upload filing

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,65 @@
-const { Router } = require('express');
-const express = require('express');
+const { Router } = require("express");
+const express = require("express");
 const router = express.Router();
-const DPC = require('../controllers/DPC');
+const DPC = require("../controllers/DPC");
 
+const path = require("path");
 
-const authController = require('../controllers/auth')
+const authController = require("../controllers/auth");
 
-router.get("/", (req, res) => {
-    if (req.session.isAuth) res.render('accueil');
-    else res.render('index');
+const multer = require("multer");
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
 });
 
+var upload = multer({ storage: storage });
 
-router.route('/EspaceDemandeur')
-    .get(DPC.get)
-    .post(DPC.post)
+var uploadMultiple = upload.fields([{ name: "file1", maxCount: 10 }]);
 
-router.get('/info',(req,res) =>{
-    if (req.session.isAuth) res.render('info');   
-    else res.render('index'); 
-})
+router.post("/uploadfile", uploadMultiple, function (req, res, next) {
+  if (req.files) {
+    console.log(req.files);
 
+    console.log("files uploaded");
+  }
+});
 
-router.route('/accueil')
-    .get((req,res) =>{
-        if (req.session.isAuth) res.render('accueil');   
-        else res.render('index');
-    })
-    .post(authController.login);
+router.get("/", (req, res) => {
+  if (req.session.isAuth) res.render("accueil");
+  else res.render("index");
+});
+
+// router.route("/EspaceDemandeur").get(DPC.get).post(DPC.post);
+router.post("/EspaceDemandeur", uploadMultiple, DPC.post);
+router.get("/EspaceDemandeur", DPC.get);
+
+router.get("/info", (req, res) => {
+  if (req.session.isAuth) res.render("info");
+  else res.render("index");
+});
+
+router.get("/test", (req, res) => {
+  if (req.session.isAuth) res.render("test");
+  else res.render("index");
+});
+
+router
+  .route("/accueil")
+  .get((req, res) => {
+    if (req.session.isAuth) res.render("accueil");
+    else res.render("index");
+  })
+  .post(authController.login);
 
 // TODO : add a /accueil with good route
-
 
 module.exports = router;
