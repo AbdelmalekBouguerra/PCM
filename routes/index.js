a242ef57a097165cfeaecbdae0000a3cf71d527f
AbdelmalekBouguerra <abdelmalekbouguerra2000@gmail.com>
2022-08-05 05:11:30 +0100

    file formating

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,28 @@
-const { Router } = require('express');
-const express = require('express');
+const { Router } = require("express");
+const express = require("express");
 const router = express.Router();
-const DPC = require('../controllers/DPC');
+const DPC = require("../controllers/DPC");
 
-
-const authController = require('../controllers/auth')
+const authController = require("../controllers/auth");
 
 router.get("/", (req, res) => {
-    if (req.session.isAuth) res.render('accueil');
-    else res.render('index');
+  if (req.session.isAuth) res.render("accueil");
+  else res.render("index");
 });
 
+router.route("/EspaceDemandeur").get(DPC.get).post(DPC.post);
 
-router.route('/EspaceDemandeur')
-    .get(DPC.get)
-    .post(DPC.post)
+router.get("/info", (req, res) => {
+  if (req.session.isAuth) res.render("info");
+  else res.render("index");
+});
 
-router.get('/info',(req,res) =>{
-    if (req.session.isAuth) res.render('info');   
-    else res.render('index'); 
-})
-
-
-router.route('/accueil')
-    .get((req,res) =>{
-        if (req.session.isAuth) res.render('accueil');   
-        else res.render('index');
-    })
-    .post(authController.login);
-
-// TODO : add a /accueil with good route
-
+router
+  .route("/accueil")
+  .get((req, res) => {
+    if (req.session.isAuth) res.render("accueil");
+    else res.render("index");
+  })
+  .post(authController.login);
 
 module.exports = router;
